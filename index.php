3f448e1098caf0fb5dea9950bce48218268242b9
Kengo Yasui <agezou@gmail.com>
2014-01-12 15:04:16 +0900

    New

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang=ja>
+  <head>
+    <meta charset="utf-8">
+    <title>開発環境</title>
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <div id="header">
+      <h1>
+        ヘッダー
+      </h1>
+      <div id="global-nav">
+        <h2>
+          ナビ 
+        </h2>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="index.php">Home</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="main">
+      <h2>
+        メイン 
+      </h2>
+      <blockquote cite="http://www.example.com">
+        <p>test1</p>
+        <p>甘さ控えめで大人向けのチョコチップクッキーをつくるには、カカオ70％ぐらいのチョコレートをつかうのがよい。</p>
+      </blockquote>
+    </div>
+    <div id="footer">
+      <h1>
+        フッター 
+      </h1>
+      <address>ページ製作者：安井 健悟</address>
+    </div>
+  </body>
+
+</html>
